a4ebdb62cffffbb90ff8da470aef14eea125ab54
Athena Boose <pestpestthechicken@yahoo.com>
2025-05-16 02:20:14 -0700

    Properly implemented an AST with (hopefully better) error handling.

--- hmacro.hs
+++ hmacro.hs
@@ -1,79 +1,83 @@
 import qualified Data.Map.Strict as Map
 import Data.Char
+import Data.List
 import Text.Show.Functions
 import Debug.Trace
 main = do print "asdf"
 
+data Token = Chr Char | NameStart | ScopeStart | ScopeEnd deriving Eq
+data Gathered = Text String | Name String | LexError String | StartScope | EndScope deriving Eq
+data ScopeElem = ScopeText String | MacroName String | Scope Scope | ParseError String deriving Eq
+type Scope = [ScopeElem]
 
+tokenize :: String -> [Token]
+tokenize ('\\':'\\':xs) = (Chr '\\'):(tokenize xs)
+tokenize ('\\':'{':xs) = (Chr '{'):(tokenize xs)
+tokenize ('\\':'}':xs) = (Chr '}'):(tokenize xs)
+tokenize ('\\':xs) = NameStart:(tokenize xs)
+tokenize ('{':xs) = ScopeStart:(tokenize xs)
+tokenize ('}':xs) = ScopeEnd:(tokenize xs)
+tokenize (x:xs) = (Chr x):(tokenize xs)
+tokenize [] = []
 
-data MacroElement = 
-        Text String
-        | Arg Int deriving Show
+isIdChar :: Token -> Bool
+isIdChar (Chr '_') = True
+isIdChar (Chr '-') = True
+isIdChar (Chr x) | isDigit x || isAsciiUpper x || isAsciiLower x = True
+isIdChar _ = False
 
-type Parsed = String
-type UnParsed = String
+isChr :: Token -> Bool
+isChr (Chr _) = True
+isChr _ = False
 
-parseText :: (String) -> (Map.Map String [MacroElement]) -> Either String (Parsed, UnParsed)
-parseText ('\\':'\\':rest) (fnmap) = do
-        (parsed, unparsed) <- parseText rest fnmap
-        Right ('\\':parsed, unparsed)
-parseText ('\\':'{':rest) (fnmap) = do
-        (parsed, unparsed) <- parseText rest fnmap
-        Right ('{':parsed, unparsed)
-parseText ('\\':'}':rest) (fnmap) = do
-        (parsed, unparsed) <- parseText rest fnmap
-        Right ('}':parsed, unparsed)
-parseText ('}':rest) (fnmap) = Right ("", '}':rest)
-parseText ('\\':xs) (fnmap) = do
-        (expanded_text, rest) <- parseMacro xs fnmap
-        (parsed, unparsed) <- parseText rest fnmap
-        Right (expanded_text ++ parsed, unparsed)
-parseText (x:xs) (fnmap) = do
-        (parsed, unparsed) <- parseText xs fnmap
-        Right(x:parsed, unparsed)
-parseText [] _ = Right ([], [])
+gather :: [Token] -> [Gathered]
+gather (ScopeStart:xs) = StartScope:(gather xs)
+gather (ScopeEnd:xs) = EndScope:(gather xs)
+gather (NameStart:Chr x:xs) | x == '_' || x == '-' || isAsciiUpper x || isAsciiLower x = 
+        let (tail, head) = span (isIdChar) xs in
+                ((Name (fmap (\(Chr c) -> c) head)):(gather tail))
+gather all@(Chr x:xs) =
+        let (tail, head) = span (isChr) all in
+                ((Text (fmap (\(Chr c) -> c) head)):(gather tail))
+gather (x:xs) = (LexError "Bad character in macro usage"):(gather xs)
+gather [] = []
 
-parseMacro :: String -> (Map.Map String [MacroElement]) -> Either String (String, String)
-parseMacro str@(x:_) (map) | isAsciiUpper x || isAsciiLower x || x == '-' || x == '_' = let (name, args_txt) = getName str in do
-        fn <- checkName name map
-        (args, rest) <- getArgs 1 args_txt map
-        expanded_text <- expandMacro fn args
-        Right (expanded_text, rest)
-parseMacro _ _ = Left ("Invalid macro name.")
+scopeInternal :: [Gathered] -> (Scope, [Gathered])
+scopeInternal ((Name name):xs) = let (rest, tail) = scopeInternal xs in
+        ((MacroName name):rest, tail)
+scopeInternal ((Text txt):xs) = let (rest, tail) = scopeInternal xs in
+        ((ScopeText txt):rest, tail)
+scopeInternal (StartScope:xs) = case scopeInternal xs of
+        (children, []) -> ((ParseError "No closing brace"):children, []) -- pretend there was no opening brace and continue parsing
+        (children, tail) -> let (rest, tail') = scopeInternal tail in
+                                ((Scope children):rest, tail')
+scopeInternal (EndScope:xs) = ([], xs)
+scopeInternal ((LexError e):xs) = let (rest, tail) = scopeInternal xs in
+        ((ParseError e):rest, tail) -- bit of a hack
+scopeInternal [] = ([], [])
 
-getName :: String -> (String, String)
-getName (x:xs) | isDigit x || isAsciiUpper x || isAsciiLower x || x == '-' || x == '_' =
-        (\(name, rest) -> ((x:name), rest)) (getName xs)
-getName (x:xs) = ([], x:xs)
-getName [] = ([], [])
+scope :: [Gathered] -> Scope
+scope s = case scopeInternal s of
+               (scp, []) -> scp
+               (scp, txt) -> scp ++ [ParseError "Extraneous closing brace"] ++ (scope txt)
 
-checkName :: String -> (Map.Map String [MacroElement]) -> Either String [MacroElement]
-checkName name map = 
-        case (Map.lookup name map) of
-                Just fn -> Right fn
-                Nothing -> Left ("Parse error: Macro `" ++ name ++ "` not defined.")
+incrementPos :: (Int, Int) -> String -> (Int, Int)
+incrementPos (lineno, colno) str = case lines str of
+                                       [x] -> (lineno, colno + (length x))
+                                       splitstr -> (lineno + (length splitstr) - 1, (length . last) splitstr)
 
--- start arg number -> unparsed txt -> identifier map -> Either err (args_map, rest)
-getArgs :: Int -> String -> (Map.Map String [MacroElement]) -> Either String (Map.Map Int String, String)
-getArgs current_num ('{':xs) (id_map) = do
-        (parsed, unparsed) <- parseText xs id_map
-        case unparsed of
-                ('}':rest) -> do
-                        (return_map, return_tail) <- getArgs (current_num + 1) rest id_map
-                        Right (Map.insert current_num parsed return_map, return_tail)
-                [] -> Left ("Failed to find ending brace when expanding a macro argument.")
-                _ -> Left ("An unknown error occurred, maybe you forgot to include a closing brace on a macro?")
-getArgs _ xs _ = Right (Map.empty, xs)
+validateScopeInternal :: (Int, Int) -> Scope -> ([(Int, Int, String)], (Int, Int))
+validateScopeInternal (lineno, colno) (ParseError e:xs) = let (errs, pos') = validateScopeInternal (lineno, colno) xs in
+        (((lineno, colno, e):errs), pos')
+validateScopeInternal pos (Scope s:xs) = let (childerrs, pos') = validateScopeInternal pos s
+                                             (sameerrs, pos'') = validateScopeInternal pos' xs in
+                                             (childerrs ++ sameerrs, pos'')
+validateScopeInternal pos@(lineno, colno) (MacroName name:xs) = validateScopeInternal (lineno, colno + 1 + (length name)) xs
+validateScopeInternal pos@(lineno, colno) (ScopeText name:xs) = validateScopeInternal (incrementPos pos name) xs
+validateScopeInternal pos _ = ([], pos)
 
-expandMacro :: [MacroElement] -> (Map.Map Int String) -> Either String String
-expandMacro [] _ = Right []
-expandMacro ((Text txt):xs) arg_values = do
-        result <- expandMacro xs arg_values
-        Right (txt ++ result)
-expandMacro ((Arg arg):xs) arg_values = do
-        arg_txt <- case (Map.lookup arg arg_values) of
-                Just txt -> Right txt
-                Nothing -> Left ("Argument `" ++ show arg ++ "` was not provided in macro invocation.")
-        rest <- expandMacro xs arg_values
-        Right (arg_txt ++ rest)
+validateScope :: Scope -> Either [(Int, Int, String)] Scope
+validateScope scp = case validateScopeInternal (1, 1) scp of
+                         ([], _) -> Right scp
+                         (errs, _) -> Left errs
 
