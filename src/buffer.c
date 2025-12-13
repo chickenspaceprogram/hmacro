e84c54d35cbe10a94d3f333f717d88a678f9f10d
Athena Boose <pestpestthechicken@yahoo.com>
2025-12-13 12:19:34 -0800

    Added buffer and taglist

--- src/buffer.c
+++ src/buffer.c
@@ -17,4 +17,86 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "buffer.h"
+#define FILL_FACTOR 2
+#define MIN_CAPACITY 16
 
+static inline uint64_t next_pwr_2(uint64_t val)
+{
+	--val;
+	val |= val >> 1;
+	val |= val >> 2;
+	val |= val >> 4;
+	val |= val >> 8;
+	val |= val >> 16;
+	val |= val >> 32;
+	++val;
+	return val;
+}
+
+int hm_buf_reserve(hm_buf *buf, uint64_t nchrs, cu_alloc *alloc)
+{
+	uint64_t next_cap = next_pwr_2(nchrs);
+	if (next_cap <= buf->capacity)
+		return 0;
+	uint8_t *new_buf = cu_malloc(next_cap, alloc);
+	if (new_buf == NULL)
+		return -1;
+	uint64_t txt_len = buf->capacity - buf->start;
+	memcpy(new_buf + next_cap - txt_len, buf->buf + buf->start, txt_len);
+	cu_free(buf->buf, buf->capacity, alloc);
+	buf->buf = new_buf;
+	buf->capacity = next_cap;
+	buf->start = next_cap - txt_len;
+	return 0;
+}
+int hm_taglist_reserve(hm_taglist *tl, uint64_t ntags, cu_alloc *alloc)
+{
+	uint64_t next_cap = next_pwr_2(ntags);
+	if (next_cap <= tl->capacity)
+		return 0;
+	hm_tag *new_buf = cu_reallocarray(tl->buf, next_cap, tl->capacity, sizeof(hm_tag), alloc);
+	if (new_buf == NULL)
+		return -1;
+	tl->buf = new_buf;
+	tl->capacity = next_cap;
+	return 0;
+}
+void hm_taglist_advance(hm_taglist *tl, cu_string_view text)
+{
+	while (text.len > 0) {
+		assert(tl->nel != 0);
+		if (tl->buf[tl->nel - 1].n_to_consume > 0) {
+			if (tl->buf[tl->nel - 1].n_to_consume > text.len) {
+				tl->buf[tl->nel - 1].n_to_consume -= text.len;
+				return;
+			}
+			else {
+				text.len -= tl->buf[tl->nel - 1].n_to_consume;
+				text.buf += tl->buf[tl->nel - 1].n_to_consume;
+				tl->buf[tl->nel - 1].n_to_consume = 0;
+			}
+			continue;
+		}
+		uint64_t tag_chrs = text.len > tl->buf[tl->nel - 1].chars_in_tag ?
+			tl->buf[tl->nel - 1].chars_in_tag :
+			text.len;
+		void *nl = memchr(text.buf, '\n', tag_chrs);
+		if (nl == NULL) {
+			text.buf += tag_chrs;
+			text.len -= tag_chrs;
+			tl->buf[tl->nel - 1].col += tag_chrs;
+			tl->buf[tl->nel - 1].chars_in_tag -= tag_chrs;
+		}
+		else {
+			uint64_t chrs_popped = (uint8_t *)nl + 1 - text.buf;
+			++tl->buf[tl->nel - 1].row;
+			tl->buf[tl->nel - 1].col = 1;
+			tl->buf[tl->nel - 1].chars_in_tag -= chrs_popped;
+			text.buf += chrs_popped;
+			text.len -= chrs_popped;
+		}
+
+		if (tl->buf[tl->nel - 1].chars_in_tag == 0)
+			hm_taglist_pop(tl);
+	}
+}
--- src/buffer.h
+++ src/buffer.h
@@ -19,21 +19,14 @@
 #pragma once
 #include <stdint.h>
 #include <stddef.h>
+#include <assert.h>
 #include <cu/string.h>
 
 typedef struct {
 	uint8_t *buf;
-	size_t capacity;
-	size_t start;
+	uint64_t capacity;
+	uint64_t start;
 } hm_buf;
-
-typedef cu_string_view hm_tag;
-
-typedef struct {
-	cu_string_view *buf;
-	size_t capacity;
-	size_t nel;
-} hm_taglist;
 
 static inline void hm_buf_init(hm_buf *buf)
 {
@@ -45,18 +38,94 @@ static inline void hm_buf_free(hm_buf *buf, cu_alloc *alloc)
 {
 	cu_free(buf->buf, buf->capacity, alloc);
 }
-int hm_buf_push(hm_buf *buf, cu_string_view txt, cu_alloc *alloc);
-void hm_buf_pop(hm_buf *buf, size_t nchrs_popped);
+int hm_buf_reserve(hm_buf *buf, uint64_t nchrs, cu_alloc *alloc);
+static inline int hm_buf_push(hm_buf *buf, cu_string_view txt, cu_alloc *alloc)
+{
+	if (txt.buf == NULL)
+		return 0;
+	int retval = hm_buf_reserve(buf, buf->capacity - buf->start + 1, alloc);
+	if (retval != 0)
+		return retval;
+	buf->start -= txt.len;
+	memcpy(buf->buf + buf->start, txt.buf, txt.len);
+	return 0;
+}
+static inline void hm_buf_pop(hm_buf *buf, uint64_t nchrs_popped)
+{
+	buf->start += nchrs_popped;
+	assert(buf->start <= buf->capacity);
+}
 static inline cu_string_view hm_buf_contents(hm_buf *buf)
 {
+	assert(buf->buf != NULL);
 	return (cu_string_view){
 		.buf = buf->buf + buf->start,
 		.len = buf->capacity - buf->start,
 	};
 }
 
-void hm_taglist_init(hm_taglist *tl);
-void hm_taglist_free(hm_taglist *buf, cu_alloc *alloc);
-int hm_taglist_push(hm_taglist *tl, hm_tag tag, cu_alloc *alloc);
-hm_tag hm_taglist_pop(hm_taglist *tl);
-hm_tag hn_taglist_peek(hm_taglist *tl);
+typedef struct {
+	// Text of the tag (usually a filename
+	cu_string_view tag;
+	// Current row and col (set these to 1 and 1
+	uint64_t row;
+	uint64_t col;
+
+	// Chars to consume before incrementing row/col
+	uint64_t n_to_consume;
+
+	// number of chars to consume while advancing row/col before removing tag
+	uint64_t chars_in_tag;
+} hm_tag;
+
+#define HM_TAG_DEFAULT(TAGTXT) (hm_tag){\
+	.tag = (TAGTXT),\
+	.row = 1,\
+	.col = 1,\
+	.n_to_consume = 0,\
+}
+
+typedef struct {
+	hm_tag *buf;
+	uint64_t capacity;
+	uint64_t nel;
+} hm_taglist;
+
+
+static inline void hm_taglist_init(hm_taglist *tl)
+{
+	tl->buf = NULL;
+	tl->capacity = 0;
+	tl->nel = 0;
+}
+static inline void hm_taglist_free(hm_taglist *tl, cu_alloc *alloc)
+{
+	cu_freearray(tl->buf, tl->capacity, sizeof(hm_tag), alloc);
+}
+int hm_taglist_reserve(hm_taglist *tl, uint64_t ntags, cu_alloc *alloc);
+static inline int hm_taglist_push(hm_taglist *tl, hm_tag tag, cu_alloc *alloc)
+{
+	int retval = hm_taglist_reserve(tl, tl->nel + 1, alloc);
+	if (retval != 0)
+		return retval;
+	tl->buf[tl->nel++] = tag;
+	return 0;
+}
+static inline hm_tag hm_taglist_pop(hm_taglist *tl)
+{
+	assert(tl->nel != 0);
+	return tl->buf[--tl->nel];
+
+}
+void hm_taglist_advance(hm_taglist *tl, cu_string_view text);
+
+// use when pushing expanded stuff onto the buffer to protect the top tag
+static inline void hm_taglist_add_consumable(hm_taglist *tl, uint64_t nconsume)
+{
+	assert(tl->nel > 0);
+	tl->buf[tl->nel - 1].n_to_consume += nconsume;
+}
+static inline hm_tag hn_taglist_peek(hm_taglist *tl)
+{
+	return tl->buf[tl->nel - 1];
+}
